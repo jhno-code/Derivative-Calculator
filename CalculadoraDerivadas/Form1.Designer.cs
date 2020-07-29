5053a5a338f1e54f4c3afc9d08d2f40116f10a59
jhno <toe.johan@gmail.com>
2020-07-28 22:53:00 -0500

    ahora se crearon 2 label un boton y un textbox

--- CalculadoraDerivadas/Form1.Designer.cs
+++ CalculadoraDerivadas/Form1.Designer.cs
@@ -28,13 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblFuncion = new System.Windows.Forms.Label();
+            this.txtFuncion = new System.Windows.Forms.TextBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnDerivar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblFuncion
+            // 
+            this.lblFuncion.AutoSize = true;
+            this.lblFuncion.Location = new System.Drawing.Point(55, 65);
+            this.lblFuncion.Name = "lblFuncion";
+            this.lblFuncion.Size = new System.Drawing.Size(48, 13);
+            this.lblFuncion.TabIndex = 0;
+            this.lblFuncion.Text = "Funcion:";
+            // 
+            // txtFuncion
+            // 
+            this.txtFuncion.Location = new System.Drawing.Point(109, 62);
+            this.txtFuncion.Name = "txtFuncion";
+            this.txtFuncion.Size = new System.Drawing.Size(520, 20);
+            this.txtFuncion.TabIndex = 1;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(327, 22);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(129, 13);
+            this.lblTitulo.TabIndex = 2;
+            this.lblTitulo.Text = "Calculadora de Derivadas";
+            // 
+            // btnDerivar
+            // 
+            this.btnDerivar.Location = new System.Drawing.Point(635, 60);
+            this.btnDerivar.Name = "btnDerivar";
+            this.btnDerivar.Size = new System.Drawing.Size(75, 23);
+            this.btnDerivar.TabIndex = 3;
+            this.btnDerivar.Text = "Derivar";
+            this.btnDerivar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btnDerivar);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.txtFuncion);
+            this.Controls.Add(this.lblFuncion);
+            this.Name = "Form1";
+            this.Text = "jhno";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblFuncion;
+        private System.Windows.Forms.TextBox txtFuncion;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnDerivar;
     }
 }
 
